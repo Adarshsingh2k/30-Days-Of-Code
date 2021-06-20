0f341b53dd757555445ee4186633680bdbf9bc95
Adarsh Singh <adarshsingh2k@gmail.com>
2021-06-20 14:41:59 +0530

    array easy and imp questions

---
+++ Arrays/SortArraysOf012.cpp
@@ -0,0 +1,130 @@
+/*
+
+Sort an array of 0s, 1s and 2s 
+Add Bookmark
+Easy Accuracy: 51.36% Submissions: 86012 Points: 2
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+Example 2:
+
+Input: 
+N = 3
+arr[] = {0 1 0}
+Output:
+0 0 1
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 <= N <= 10^6
+0 <= A[i] <= 2
+
+Company Tags
+*/
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int arr[], int n)
+    {
+        // coode here 
+         int i, cnt0 = 0, cnt1 = 0, cnt2 = 0;
+ 
+    // Count the number of 0s, 1s and 2s in the array
+    for (i = 0; i < n; i++) {
+        switch (arr[i]) {
+        case 0:
+            cnt0++;
+            break;
+        case 1:
+            cnt1++;
+            break;
+        case 2:
+            cnt2++;
+            break;
+        }
+    }
+ 
+    // Update the array
+    i = 0;
+ 
+    // Store all the 0s in the beginning
+    while (cnt0 > 0) {
+        arr[i++] = 0;
+        cnt0--;
+    }
+ 
+    // Then all the 1s
+    while (cnt1 > 0) {
+        arr[i++] = 1;
+        cnt1--;
+    }
+ 
+    // Finally all the 2s
+    while (cnt2 > 0) {
+        arr[i++] = 2;
+        cnt2--;
+    }
+ 
+    // Print the sorted array
+    //printArr(arr, n);
+    }
+    
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
