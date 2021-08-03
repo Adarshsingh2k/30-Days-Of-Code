9e246bcd07b9cceec77b63a0aed46d7dabb7d418
Adarsh Singh <adarshsingh2k@gmail.com>
2021-08-03 21:25:18 +0530

    Graph-> Dikjstars Algo

--- Graph/DikjstrasAlgo.cpp
+++ Graph/DikjstrasAlgo.cpp
@@ -37,7 +37,7 @@ You don't need to read input or print anything. Your task is to complete the fun
 Expected Time Complexity: O(V2).
 Expected Auxiliary Space: O(V2).
 
- 
+ .<? g.hg,./  lb/n.,k
 */
 
 // { Driver Code Starts
@@ -130,3 +130,54 @@ int main()
 }
 
   // } Driver Code Ends
+
+/*//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m,source;
+    cin >> n >> m;
+    vector<pair<int,int> > g[n+1];  // 1-indexed adjacency list for of graph
+
+    int a,b,wt;
+    for(int i = 0; i<m ; i++){
+        cin >> a >> b >> wt;
+        g[a].push_back(make_pair(b,wt));
+        g[b].push_back(make_pair(a,wt));
+    }   
+    
+    cin >> source;
+    
+    // Dijkstra's algorithm begins from here
+    priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > pq;// min-heap ; In pair => (dist,from)
+    vector<int> distTo(n+1,INT_MAX);    // 1-indexed array for calculating shortest paths; 
+    
+    distTo[source] = 0;
+    pq.push(make_pair(0,source));   // (dist,from)
+    
+    while( !pq.empty() ){
+        int dist = pq.top().first;
+        int prev = pq.top().second;
+        pq.pop();
+        
+        vector<pair<int,int> >::iterator it;
+        for( it = g[prev].begin() ; it != g[prev].end() ; it++){
+            int next = it->first;
+            int nextDist = it->second;
+            if( distTo[next] > distTo[prev] + nextDist){
+                distTo[next] = distTo[prev] + nextDist;
+                pq.push(make_pair(distTo[next], next));
+            }
+        }
+        
+    }
+    
+    cout << "The distances from source, " << source << ", are : \n";
+    for(int i = 1 ; i<=n ; i++) cout << distTo[i] << " ";
+    cout << "\n";
+    
+    return 0;
+
+*/
