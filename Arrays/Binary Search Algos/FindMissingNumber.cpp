28df0dce56946156494032daf179a05916e7749f
Adarsh Singh <adarshsingh2k@gmail.com>
2021-09-04 23:07:03 +0530

    Array-> Find Missing Number

---
+++ Arrays/Binary Search Algos/FindMissingNumber.cpp
@@ -0,0 +1,80 @@
+/*
+Missing number in array 
+Easy Accuracy: 42.51% Submissions: 100k+ Points: 2
+Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,3,5}
+Output: 4
+Example 2:
+
+Input:
+N = 10
+A[] = {1,2,3,4,5,6,7,8,10}
+Output: 9
+
+Your Task :
+You don't need to read input or print anything. Complete the function MissingNumber() that takes array and N as input  parameters and returns the value of the missing number.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ A[i] ≤ 106
+*/
+
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+ // } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& a, int n) {
+        // Your code goes here
+        
+        int out;
+        int sum=0;
+        
+        for(int i=0;i<a.size();i++){
+            sum+=a[i];
+            
+           
+        }
+       // cout<<sum<<endl;
+        
+        out= (n*(n+1)/2)-sum;
+        
+        return out;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
