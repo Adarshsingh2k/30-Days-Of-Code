271404dddda1bfa7acaf9d443c31c4f0f471a83f
Adarsh <adarshsingh2k@gmail.com>
2022-07-07 23:03:48 +0530

    nonrepeatingCharacter

---
+++ String/First Non Repeating Character.cpp
@@ -0,0 +1,89 @@
+/*
+Non Repeating Character 
+Easy Accuracy: 49.1% Submissions: 63053 Points: 2
+Given a string S consisting of lowercase Latin Letters. Return the first non-repeating character in S. If there is no non-repeating character, return '$'.
+
+Example 1:
+
+Input:
+S = hello
+Output: h
+Explanation: In the given string, the
+first character which is non-repeating
+is h, as it appears first and there is
+no other 'h' in the string.
+Example 2:
+
+Input:
+S = zxvczbtxyzvy
+Output: c
+Explanation: In the given string, 'c' is
+the character which is non-repeating. 
+Your Task:
+You only need to complete the function nonrepeatingCharacter() that takes string S as a parameter and returns the character. If there is no non-repeating character then return '$' .
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Number of distinct characters).
+Note: N = |S|
+
+Constraints:
+1 <= N <= 103
+
+ 
+*/
+
+// Ref Article-> https://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+       //Your code here
+       int collect[256]={0};
+       for(int i=0;i<S.length();i++){
+           collect[S[i]]++;
+       }
+       for(int i=0;i<S.length();i++){
+           if(collect[S[i]]==1) return S[i];
+       }
+       
+       return '$';
+       
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--)
+	{
+	
+	    string S;
+	    cin >> S;
+	    Solution obj;
+        char ans = obj.nonrepeatingCharacter(S);
+        
+        if(ans != '$')
+	    cout << ans;
+        else cout << "-1";
+            
+        cout << endl;
+	    
+	}
+	
+	return 0;
+}
+  // } Driver Code Ends
